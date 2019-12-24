305f821b501f9d41c8012eb011627ef19f3968a6
Jordan Holberg <1364262+EvilJordan@users.noreply.github.com>
2019-12-24 18:33:02 -0500

    fixed path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var path = require('path');
         name: 'js',
         entry: sgmfScripts.createJsPath(),
         output: {
-            path: path.resolve('./plugin_spankpay/cartridge/static'),
+            path: path.resolve('./cartridges/plugin_spankpay/cartridge/static'),
             filename: '[name].js'
         },
         resolve: {
@@ -20,7 +20,7 @@ var path = require('path');
         name: 'scss',
         entry: sgmfScripts.createScssPath(),
         output: {
-            path: path.resolve('./plugin_spankpay/cartridge/static'),
+            path: path.resolve('./cartridges/plugin_spankpay/cartridge/static'),
             filename: '[name].css'
         },
         module: {
