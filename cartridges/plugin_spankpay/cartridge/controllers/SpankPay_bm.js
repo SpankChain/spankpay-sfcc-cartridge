67614b925ed61dcd15dbb6491e4663d6cca4c5f0
EvilJordan <eviljordan@aol.com>
2019-12-27 19:58:36 -0500

    changed scopes

--- cartridges/plugin_spankpay/cartridge/controllers/SpankPay_bm.js
+++ cartridges/plugin_spankpay/cartridge/controllers/SpankPay_bm.js
@@ -5,13 +5,13 @@ const boguard = require('~/cartridge/scripts/boguard');
 function Dashboard() { //business manager merchant dashboard
     let pdict = {};
     let template = '/spankpay/dashboard';
-    const ISML = require('dw/template/ISML');
+    let ISML = require('dw/template/ISML');
     ISML.renderTemplate(template, pdict);
 }
 
 function Prefs() {
-    var URLUtils = require('dw/web/URLUtils');
-    var csrfProtection = require('dw/web/CSRFProtection');
+    let URLUtils = require('dw/web/URLUtils');
+    let csrfProtection = require('dw/web/CSRFProtection');
     let url = URLUtils.url('ViewApplication-BM','csrf_token',csrfProtection.generateToken());
     response.redirect(url+'#/?preference#site_preference_group_attributes!id!SPANKPAY');
 }
