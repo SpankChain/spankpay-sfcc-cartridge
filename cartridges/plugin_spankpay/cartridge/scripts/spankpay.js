30be482c4d851db3dbd573ac1fab0b71332a7ca3
Jordan Holberg <1364262+EvilJordan@users.noreply.github.com>
2020-01-19 03:04:29 -0500

    type hinting SFCC style

--- cartridges/plugin_spankpay/cartridge/scripts/spankpay.js
+++ cartridges/plugin_spankpay/cartridge/scripts/spankpay.js
@@ -1,12 +1,19 @@
-/*
-SpankPay utility library
-modified to work with SFCC
-docs.spankpay.com
+/**
+* SpankPay utility library modified to work with SFCC - docs.spankpay.com
 */
+
+'use strict';
 
 const cryptoLib = require('dw/crypto');
 const crypto = cryptoLib.Mac('HmacSHA256');
 
+/**
+ * Decode the SpankPay webhook
+ * @param {String} secret - Secret Key
+ * @param {String} sig - SpankPay signature querystring
+ * @param {String} data - SpankPay payload JSON.strigified
+ * @return {Object} - Returns the data object or an object containing the decoding error
+ */
 function decodeSpankPayWebhook(secret, sig, data) {
     if (!data || data.slice(0, 1) != '{') {
         const msg = (
@@ -43,12 +50,25 @@ function decodeSpankPayWebhook(secret, sig, data) {
     return [dataObj, timestamp, null];
 }
 
+/**
+ * Trim the SpankPay data
+ * @param {String} s - Data
+ * @param {String} len - Chunk length
+ * @return {String} - Returns the shortened data string
+ */
 function shorten(s, len) {
     if (!len) { len = 16; }
     if (!s || s.length <= len) { return s; }
     return s.slice(0, len / 2) + '…' + s.slice(s.length - len / 2);
 }
 
+/**
+ * Sign SpankPay data
+ * @param {String} secret - Secret Key
+ * @param {String} data - Data to sign
+ * @param {String} t - Timestamp
+ * @return {String} - Returns a valid signature and timestamp string
+ */
 function signSpankPayData(secret, data, t) {
     if (t === undefined) { t = parseInt(Date.now() / 1000); }
     const hash = crypto.digest(t + '.' + data, secret);
